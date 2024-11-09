3de3bcb252b2c9cc67736e811156829caad0b783
Derek Garnett <rizzagum@gmail.com>
2024-11-09 06:58:41 +0000

    ADD project listing API and URL routing

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import APIview
+from rest_framework.response import Response
+from .models import Project
 
 # Create your views here.
+class ProjectList(APIview):
+    def get(self, request):
+        projects = Project.objects.all()
+        return Response(projects)
+    
