c673afb272a8fd839a8b522aab091d36a7cb56bb
Derek Garnett <rizzagum@gmail.com>
2024-11-09 06:38:20 +0000

    REMOVE unused project creation signal for new users in models.py

--- projects/models.py
+++ projects/models.py
@@ -12,7 +12,9 @@ class Project(models.Model):
 
     def __str__(self):
         return f"{self.owner}'s projects"
-    
-    def create_project(sender, instance, created, **kwargs):
-        if created:
-            Project.objects.create(owner=instance)
+   
+# This signal will create a project for every new user that is created 
+# post_save.connect(create_project, sender=User)  
+# def create_project(sender, instance, created, **kwargs):
+#     if created:
+#         Project.objects.create(owner=instance)
