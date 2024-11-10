5b7c7167c76ff17e836d4a4a20ab40fc260162c9
Derek Garnett <rizzagum@gmail.com>
2024-11-10 16:00:12 +0000

    MODELS: allow projectURL to be blank or null in Project model

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=200)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    projectURL = models.URLField(max_length=200)
+    projectURL = models.URLField(max_length=200, blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='projects/images/', blank=True, null=True)
 
