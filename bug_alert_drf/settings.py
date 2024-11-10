7d9db659851623b3963139c3c586542dfbb4da18
Derek Garnett <rizzagum@gmail.com>
2024-11-10 07:04:35 +0000

    SETTINGS: toggle DEBUG mode based on environment variable

--- bug_alert_drf/settings.py
+++ bug_alert_drf/settings.py
@@ -34,8 +34,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost', 'bug-alert-drf-7540ff833a9e.herokuapp.com', 
