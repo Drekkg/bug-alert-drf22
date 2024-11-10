a303518ed781ffe9accdac06b266e9884412a277
Derek Garnett <rizzagum@gmail.com>
2024-11-10 15:47:32 +0000

    VIEWS: implement POST method for ProjectList to create new projects

--- projects/views.py
+++ projects/views.py
@@ -5,14 +5,6 @@ from .models import Project
 from .serializers import ProjectSerializer
 
 
-# Create your views here.
-# class UserCreate(APIView):
-#     def post(self, request, format=None):
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -23,3 +15,10 @@ class ProjectList(APIView):
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
     
+    
+    def post(self, request):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
