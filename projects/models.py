1841f2564236bdfb38f1b8a74fad1d31a416e3eb
Derek Garnett <rizzagum@gmail.com>
2024-11-10 20:24:06 +0000

    models: add ordering to Project model by created_at field

--- projects/models.py
+++ projects/models.py
@@ -8,8 +8,9 @@ class Project(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     projectURL = models.URLField(max_length=200 , blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
- 
 
+    class Meta:
+        ordering = ['-created_at']
     def __str__(self):
         return f"{self.owner.username}'s projects"
    
