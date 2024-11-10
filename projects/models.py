9775a605bf8aa5b2cbd10abdc09b6ca1245e169d
Derek Garnett <rizzagum@gmail.com>
2024-11-10 20:26:57 +0000

    models: update ordering in Project model to use created_on field

--- projects/models.py
+++ projects/models.py
@@ -10,7 +10,7 @@ class Project(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['-created_on']
     def __str__(self):
         return f"{self.owner.username}'s projects"
    
