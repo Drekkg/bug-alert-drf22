b8674bc316fd4779cecdf978ff2d169f5b16f937
Derek Garnett <rizzagum@gmail.com>
2024-11-10 16:39:08 +0000

    MODELS: remove 'image' field from Project model and update ProjectSerializer fields

--- projects/models.py
+++ projects/models.py
@@ -8,7 +8,7 @@ class Project(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     projectURL = models.URLField(max_length=200, blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='projects/images/', blank=True, null=True)
+
 
     def __str__(self):
         return f"{self.owner}'s projects"
--- projects/serializers.py
+++ projects/serializers.py
@@ -24,4 +24,4 @@ class ProjectSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')  
     class Meta:
         model = Project
-        fields = ['id', 'title', 'description', 'projectURL', 'created_on', 'image', 'owner']
+        fields = ['id', 'title', 'description', 'projectURL', 'created_on',  'owner']
