ffa70fc93fde6691dccd5ff207335e2cf772f3dd
Derek Garnett <rizzagum@gmail.com>
2024-11-11 19:49:46 +0000

    FEAT: update issue list API to filter by project ID; modify URL routing and enhance priority field choices in migrations

--- issues/urls.py
+++ issues/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from issues import views
 # from .views import UserCreate
 
-
 urlpatterns = [
-    path('issues/<int:project_id', views.IssueList.as_view()),
-    
+    path('issues/project/<int:project_id>/', views.IssueList.as_view(), name='issue-list-by-project'),
 ]
--- issues/views.py
+++ issues/views.py
@@ -2,21 +2,20 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
 from .models import Issue
 from .serializers import IssueSerializer
 
 
 class IssueList(APIView):
-    def get(self, request):
-        issues = Issue.objects.all()
+    def get(self, request, project_id):
+        issues = Issue.objects.filter(issue_project_id=project_id)
         serializer = IssueSerializer(issues, many=True)
         return Response(serializer.data)
     
     
-    def post(self, request):
+    def post(self, request, project_id):
         serializer = IssueSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(owner=request.user)
+            serializer.save(owner=request.user, issueproject_id=project_id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
