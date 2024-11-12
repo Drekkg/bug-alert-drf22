37d744aa2b95dbb3e8d63630c9ffb0ad7e8a5fe7
Derek Garnett <rizzagum@gmail.com>
2024-11-12 08:25:41 +0000

    FIX issues models and added admin imports and registered issue app

--- issues/models.py
+++ issues/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from projects.models import Project
 
 from django.db.models.signals import post_save
 
@@ -11,17 +12,20 @@ PRIORITY_CHOICES = {
     ("critical", "critical"),
 }
 
+
 class Issue(models.Model):
     issue = models.CharField(max_length=200)
     console_error = models.TextField(max_length=300)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     repeatable = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default="low")
-    issue_project = models.ForeignKey('projects.Project', on_delete=models.CASCADE)
+    priority = models.CharField(
+        max_length=10, choices=PRIORITY_CHOICES, default="low")
+    issue_project = models.ForeignKey(
+        Project, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['-created_on']
+
     def __str__(self):
         return f"{self.owner.username}'s issues"
-
--- issues/serializers.py
+++ issues/serializers.py
@@ -2,17 +2,19 @@ from rest_framework import serializers
 from .models import Issue
 from django.contrib.auth.models import User
 
+
 class IssueSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')  
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Issue
         fields = [
-            'id', 
-            'issue', 
-            'console_error', 
-            'owner', 
-            'repeatable', 
-            'created_on', 
-            'priority', 
+            'id',
+            'issue',
+            'console_error',
+            'owner',
+            'repeatable',
+            'created_on',
+            'priority',
             'issue_project',
         ]
