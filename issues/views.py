01515123f1080fd6641db4b004f110d7c3290909
Derek Garnett <rizzagum@gmail.com>
2024-11-12 17:47:21 +0000

    UPDATE issues view

--- issues/views.py
+++ issues/views.py
@@ -12,11 +12,20 @@ class IssueList(APIView):
         serializer = IssueSerializer(issues, many=True)
         return Response(serializer.data)
 
-    def post(self, request, project_id):
-        serializer = IssueSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(owner=request.user, issueproject_id=project_id)
-            # serializer.save(owner=request.user)
+    # def post(self, request, project_id):
+    #     serializer = IssueSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save(owner=request.user, issueproject_id=project_id)
+        # serializer.save(owner=request.user)
 
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, project_id):
+        data = request.data
+        data['issue_project_id'] = project_id  # Add project_id to the data
+        serializer = IssueSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
