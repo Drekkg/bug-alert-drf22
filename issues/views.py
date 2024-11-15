e8a125940df4d2de23f4d09b0049a71b8c606592
Derek Garnett <rizzagum@gmail.com>
2024-11-15 21:16:34 +0000

    ADD adds issue_id to comments view

--- issues/views.py
+++ issues/views.py
@@ -7,23 +7,13 @@ from .serializers import IssueSerializer
 
 class IssueList(APIView):
     def get(self, request, project_id):
-        # issues = Issue.objects.filter(issue_project_id=project_id)
         issues = Issue.objects.all()
         serializer = IssueSerializer(issues, many=True)
         return Response(serializer.data)
 
-    # def post(self, request, project_id):
-    #     serializer = IssueSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save(owner=request.user, issueproject_id=project_id)
-        # serializer.save(owner=request.user)
-
-        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
     def post(self, request, project_id):
         data = request.data
-        data['issue_project_id'] = project_id  # Add project_id to the data
+        data['issue_project_id'] = project_id
         serializer = IssueSerializer(data=data)
         if serializer.is_valid():
             serializer.save(owner=request.user)
