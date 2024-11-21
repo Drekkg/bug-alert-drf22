41a8cac605e602a1619a240ca711471ddbe1e11f
Derek Garnett <rizzagum@gmail.com>
2024-11-21 17:12:50 +0000

    UPDATE URL to accept pk as int

--- issues/urls.py
+++ issues/urls.py
@@ -3,7 +3,8 @@ from issues import views
 # from .views import UserCreate
 
 urlpatterns = [
-    path('issues/', views.IssueList.as_view()),
+    path('isssues/<int:pk>', views.IssueList.as_view()),
     path('issues/<int:project_id>/',
          views.IssueList.as_view(), name='issue-list-by-project'),
+    path('issues/', views.IssueList.as_view()),
 ]
